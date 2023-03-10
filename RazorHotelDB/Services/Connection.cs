e49837de5b202a688ebe0efd6217a75a319664a2
Frederik Baun Grubak <fred00s7@edu.zealand.dk>
2023-03-10 10:40:02 +0100

    Add project files.

---
+++ RazorHotelDB/Services/Connection.cs
@@ -0,0 +1,14 @@
+﻿namespace RazorHotelDB.Services
+{
+    public abstract class Connection
+    {
+        protected String connectionString;
+        public IConfiguration Configuration { get; }
+
+        public Connection(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+        }
+    }
+}
---
+++ RazorHotelDB/Services/HotelService.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Data.SqlClient;
+using RazorHotelDB.Interfaces;
+using RazorHotelDB.Models;
+
+namespace RazorHotelDB.Services
+{
+    public class HotelService : Connection, IHotelService
+    {
+        private string queryString = "select * from Hotel";
+
+        public HotelService(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public Task<bool> CreateHotelAsync(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Hotel> DeleteHotelAsync(int hotelNr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Hotel>> GetAllHotelAsync()
+        {
+            List<Hotel> hoteller = new List<Hotel>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(queryString, connection))
+                {
+                    try
+                    {
+                        await command.Connection.OpenAsync();//aSynkront
+                        Thread.Sleep(1000);
+                        SqlDataReader reader = await command.ExecuteReaderAsync();//aSynkront
+                        Thread.Sleep(1000);
+                        while (await reader.ReadAsync())
+                        {
+                            int hotelNr = reader.GetInt32(0);
+                            String hotelNavn = reader.GetString(1);
+                            String hotelAdr = reader.GetString(2);
+                            Hotel hotel = new Hotel(hotelNr, hotelNavn, hotelAdr);
+                            hoteller.Add(hotel);
+                        }
+                    }
+                    catch (SqlException sqlEx)
+                    {
+                        Console.WriteLine("Database error " + sqlEx.Message);
+                        return null;
+                    }
+                    catch (Exception exp)
+                    {
+                        Console.WriteLine("Generel fejl" + exp.Message);
+                        return null;
+                    }
+                }
+            }
+            return hoteller;
+        }
+
+
+        public Task<Hotel> GetHotelFromIdAsync(int hotelNr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Hotel>> GetHotelsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateHotelAsync(Hotel hotel, int hotelNr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
